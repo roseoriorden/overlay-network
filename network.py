ddc58388ee572752f716c837a4c1c12e06e96094
Rosemarie O'Riorden <roriorden@redhat.com>
2022-03-21 15:42:57 -0400

    TCP client-server connection

--- network.py
+++ network.py
@@ -10,11 +10,8 @@ from socketserver import BaseRequestHandler, TCPServer
 import hashlib
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description='Basic python networking example')
-parser.add_argument('--alt', action='store_true')
-args = parser.parse_args()
-
 own_ip = None
+server_ip = None
 
 
 def init_ip():
@@ -50,9 +47,7 @@ def tcp_listener(port):
         server.shutdown()
 
 
-def tcp_client(port, data):
-    host_ip = own_ip
-
+def tcp_client(port, data, server_ip):
     # Initialize a TCP client socket using SOCK_STREAM
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cntx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -60,25 +55,36 @@ def tcp_client(port, data):
     cntx.load_cert_chain('cert.pem')
 
     s = cntx.wrap_socket(s, server_hostname='test.server')
-
-    try:
-        # Establish connection to TCP server and exchange data
-        s.connect((host_ip, port))
-        print('TCP connection established with ' + host_ip)
-        s.sendall(data.encode())
-        # Read data from the TCP server and close the connection
-        received = s.recv(1024)
-    finally:
-        s.close()
-
-    print(f"Bytes Sent:     {data}")
-    print(f"Bytes Received: {received.decode()}")
-
+    print("server IP in tcp_client: " + str(server_ip))
+    print_server_ip()
+    while server_ip:
+        try:
+            # Establish connection to TCP server and exchange data
+            s.connect((server_ip, port))
+            print('TCP connection established with ' + server_ip)
+            s.sendall(data.encode())
+            # Read data from the TCP server and close the connection
+            received = s.recv(1024)
+            break
+        finally:
+            s.close()
+        break
+    if not server_ip:
+        print("no server IP found")
+    if data:
+        print("Bytes Sent:     {data}")
+    else:
+        print("No data sent")
+    if received:
+        print(f"Bytes Received: {received.decode()}")
+    else:
+        print("no data received")
 
 #######################################
 #          Broadcast Example          #
 #######################################
 def broadcast_listener(socket):
+    global server_ip
     hash = get_bcast_hash()
     try:
         while True:
@@ -87,7 +93,12 @@ def broadcast_listener(socket):
                 string_rec = data.decode("utf-8")
                 #substrings_rec = string_rec.split()
                 if string_rec == hash:
-                    print("Broadcast received: " + string_rec)
+                    server_ip = addr[0]
+                    print("Broadcast received from " + server_ip)
+                    print("server_ip in broadcast_listener: " + server_ip)
+                    print_server_ip()
+                    return server_ip
+                    #now needs to connect with tcp server
                     #if int(substrings_rec[1]) >= int(get_current_seconds()) - 5 and int(substrings_rec[1]) <= int(get_current_seconds()):
                         #print("time validated")
                     
@@ -123,24 +134,31 @@ def get_current_date_time():
 def get_current_seconds():
     return str(time.time()).split('.')[0]
 
+def print_server_ip():
+    global server_ip
+    server_ip = server_ip
+    print("sever IP in helper func: " + str(server_ip))
+
 #######################################
 #               Driver                #
 #######################################
 def communication_manager():
+    global server_ip
     # find own ip
     init_ip()
 
-    bcast_port = 1337 if args.alt else 1338
-    tcp_listen = 9990 if args.alt else 9995
-    tcp_port = 9995 if args.alt else 9990
+    bcast_port = 1337
+    tcp_listen = 9990
+    tcp_port = 9995
 
     # broadcast to other users that you exist
     broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     broadcast_socket.bind(('', bcast_port))
+    broadcast_listener(broadcast_socket)
     procs = []
-    procs.append(Process(target=broadcast_listener,
-                         name="broadcast_listener_worker",
-                         args=(broadcast_socket,)))
+    #procs.append(Process(target=broadcast_listener,
+                         #name="broadcast_listener_worker",
+                         #args=(broadcast_socket,)))
 
     procs.append(Process(target=broadcast_sender,
                  name="broadcast_sender_worker",
@@ -155,7 +173,7 @@ def communication_manager():
             print("Starting: {}".format(p.name))
             p.start()
         while True:
-            tcp_client(tcp_port, input("Enter message to send: "))
+            tcp_client(tcp_port, input("Enter message to send: "), server_ip)
             sleep(1)
 
     except KeyboardInterrupt:
